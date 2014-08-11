eabdf51058a301228200ed4cfb5fa24c0d05cb38
Daniel Schepers <Daniel@dan-on-mercury.enova.com>
2014-08-11 10:16:02 -0500

    Timestamp with time zone -> timestamptz, varchar(200) -> text

--- yadda/create_tables.sql
+++ yadda/create_tables.sql
@@ -14,8 +14,8 @@ create table users(
   state char(2)  not null,
   zipcode int  not null,
   birthday date not null,
-  created_at timestamp with time zone default now(),
-  updated_at timestamp with time zone 
+  created_at timestamptz default now(),
+  updated_at timestamptz 
   --Doesn't make sense to have created/updated by for user
 );
 
@@ -28,31 +28,32 @@ create table breweries(
   city varchar(40) not null,
   state char(2)  not null,
   zipcode int  not null,
-  description varchar(200),
+  description text,
   founding_year int,
   created_by int references users(id) not null,
   updated_by int references users(id),
-  created_at timestamp with time zone default now(),
-  updated_at timestamp with time zone
+  created_at timestamptz default now(),
+  updated_at timestamptz
 ); 
 
 create table beers(
   id serial primary key,
+  name varchar(30) not null,
   brewery_id int references breweries(id) on delete restrict,
   style varchar(30)  not null,
-  description varchar(200),
+  description text,
   brewing_year int,  
   created_by int references users(id) not null,
   updated_by int references users(id),
-  created_at timestamp with time zone default now(),
-  updated_at timestamp with time zone
+  created_at timestamptz default now(),
+  updated_at timestamptz
 );
 
 create table ratings(
   user_id int references users(id) on delete set null,
   beer_id int references beers(id) on delete cascade,
-  rating float not null,
-  created_at timestamp with time zone default now(),
-  updated_at timestamp with time zone  -- A rating should always be created/updated by the user already associated with it. Better to enforce that at app level than have repetative fields here
+  rating float not null-- a rating from 0 to 5. Ratings from users will probably be simple reflections of "How did I like it?" but there are more complex methods: http://en.wikipedia.org/wiki/Beer_rating,
+  created_at timestamptz default now(),
+  updated_at timestamptz  -- A rating should always be created/updated by the user already associated with it. Better to enforce that at app level than have repetative fields here
 );
 
